32be942b63ca5feaeff551399b2871a70266ddd6
Feross Aboukhadijeh <feross@feross.org>
2015-08-10 14:01:08 +0200

    cmd: set options via command line

--- bin/cmd.js
+++ bin/cmd.js
@@ -6,25 +6,48 @@ var minimist = require('minimist')
 var argv = minimist(process.argv.slice(2), {
   alias: {
     o: 'outfile',
+    n: 'name',
     h: 'help'
-  }
+  },
+  boolean: [
+    'help'
+  ],
+  string: [
+    'outfile',
+    'name',
+    'creationDate',
+    'comment',
+    'createdBy',
+    'announce',
+    'urlList'
+  ]
 })
 
 var infile = argv._[0]
 var outfile = argv.outfile
 
 if (!infile || argv.help) {
-  console.log('usage: create-torrent <directory OR file> {-o outfile.torrent}')
+  console.log('usage: create-torrent <directory OR file> [OPTIONS]')
   console.log('')
   console.log('Create a torrent file from a directory or file.')
   console.log('')
   console.log('If an output file isn\'t specified with `-o`, the torrent file will be ')
   console.log('written to stdout.')
   console.log('')
+  console.log('-o, --outfile    Output file. If not specified, stdout is used [string]')
+  console.log('-n, --name       Torrent name [string]')
+  console.log('--creationDate   Creation date [Date]')
+  console.log('--comment        Torrent comment [string]')
+  console.log('--createdBy      Created by client [string]')
+  console.log('--private        Private torrent? [boolean] [default: false]')
+  console.log('--pieceLength    Piece length [number] [default: reasonable length]')
+  console.log('--announce       Tracker url [string] [default: reasonable trackers]')
+  console.log('--urlList        Web seed url [string]')
+  console.log('')
   process.exit(0)
 }
 
-createTorrent(infile, function (err, torrent) {
+createTorrent(infile, argv, function (err, torrent) {
   if (err) {
     console.error(err.stack)
     process.exit(1)
