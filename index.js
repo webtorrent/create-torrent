d7321ce22989d1e64358dd59a7dc12939cd1ae51
Feross Aboukhadijeh <feross@feross.org>
2015-11-30 22:53:27 -0800

    remove dead trackers from the defaults list

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 module.exports = createTorrent
 
 module.exports.announceList = [
-  [ 'udp://tracker.publicbt.com:80' ],
   [ 'udp://tracker.openbittorrent.com:80' ],
-  [ 'udp://open.demonii.com:1337' ],
   [ 'udp://tracker.webtorrent.io:80' ],
   [ 'wss://tracker.webtorrent.io' ] // For WebRTC peers (see: WebTorrent.io)
 ]
