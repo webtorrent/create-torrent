1e27dafcf7f3215d5ed6c09627c2e1464d59472b
Diego Rodríguez Baquero <DiegoRBaquero@users.noreply.github.com>
2015-12-20 20:38:20 -0500

    Added βTorrent tracker

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ module.exports.announceList = [
   [ 'udp://tracker.leechers-paradise.org:6969' ],
   [ 'udp://tracker.coppersurfer.tk:6969' ],
   [ 'udp://exodus.desync.com:6969' ],
-  [ 'wss://tracker.webtorrent.io' ] // For WebRTC peers (see: WebTorrent.io)
+  [ 'wss://tracker.webtorrent.io' ], // For WebRTC peers (see: WebTorrent.io)
+  [ 'wss://tracker.btorrent.xyz' ] // For WebRTC peers (see: btorrent.xyz)
 ]
 
 var bencode = require('bencode')
