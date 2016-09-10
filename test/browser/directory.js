521cdd4b77ae35ec60a3dcb5b7ff9a2b3e32728a
Feross Aboukhadijeh <feross@feross.org>
2016-09-10 11:18:38 -0700

    test: fix errors

--- test/browser/directory.js
+++ test/browser/directory.js
@@ -1,7 +1,6 @@
 /* global Blob */
 
 var createTorrent = require('../../')
-var fixtures = require('webtorrent-fixtures')
 var fs = require('fs')
 var parseTorrent = require('parse-torrent')
 var path = require('path')
@@ -15,9 +14,10 @@ function makeFileShim (buf, name, fullPath) {
   return file
 }
 
-var numbers1 = makeFileShim(fs.readFileSync(path.join(fixtures.numbers.contentPath, '1.txt'), 'utf8'), '1.txt', '/numbers/1.txt')
-var numbers2 = makeFileShim(fs.readFileSync(path.join(fixtures.numbers.contentPath, '2.txt'), 'utf8'), '2.txt', '/numbers/2.txt')
-var numbers3 = makeFileShim(fs.readFileSync(path.join(fixtures.numbers.contentPath, '3.txt'), 'utf8'), '3.txt', '/numbers/3.txt')
+var numbers1 = makeFileShim(fs.readFileSync(path.join(__dirname, '../../node_modules/webtorrent-fixtures/fixtures/numbers/1.txt'), 'utf8'), '1.txt', 'numbers/1.txt')
+var numbers2 = makeFileShim(fs.readFileSync(path.join(__dirname, '../../node_modules/webtorrent-fixtures/fixtures/numbers/2.txt'), 'utf8'), '2.txt', 'numbers/2.txt')
+var numbers3 = makeFileShim(fs.readFileSync(path.join(__dirname, '../../node_modules/webtorrent-fixtures/fixtures/numbers/3.txt'), 'utf8'), '3.txt', 'numbers/3.txt')
+
 var DSStore = makeFileShim('blah', '.DS_Store', '/numbers/.DS_Store') // this should be ignored
 
 test('create multi file torrent with directory at root', function (t) {
